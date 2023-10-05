bef59e0f2f0066cccfd311d6c40097269030ec8c
Pu Zhao <pu.zhao23@imperial.ac.uk>
2023-10-05 11:45:18 +0100

    Full first commit of new project

---
+++ week1/code/boilerplate.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Author: Your Name your.login@imperial.ac.uk
+# Script: boilerplate.sh
+# Desc: simple boilerplate for shell scripts
+# Arguments: none
+# Date: Oct 2019
+
+echo -e "\nThis is a shell script! \n"
+
+#exit
