c2a1f6a1c6d3620bd532e4309ee2a92689f57bd1
Pu Zhao <pu.zhao23@imperial.ac.uk>
2023-10-19 15:22:45 +0100

    update cfex*1.py

--- week2/code/cfexercises1.py
+++ week2/code/cfexercises1.py
@@ -3,25 +3,25 @@
 """Some functions exemplifying the use of control statements"""
 # docstrings are considered part of the running code (normal comments are
 # stripped). Hence, you can access your docstrings at run time.
-__author__ = 'Samraat Pawar (s.pawar@imperial.ac.uk)'
+__author__ = 'Pu Zhao (pu.zhao@imperial.ac.uk)'
 __version__ = '0.0.1'
 
 import sys
 
 
-def foo_1(x):
+def foo_1(x=10):
     x = float(x)
     return x ** 0.5
 
 
-def foo_2(x, y):
+def foo_2(x=10, y=10):
     x, y = float(x), float(y)
     if x > y:
         return x
     return y
 
 
-def foo_3(x, y, z):
+def foo_3(x=30, y=20, z=10):
     x, y, z = float(x), float(y), float(z)
     if x > y:
         tmp = y
@@ -34,7 +34,7 @@ def foo_3(x, y, z):
     return [x, y, z]
 
 
-def foo_4(x):
+def foo_4(x=10):
     x = int(x)
     result = 1
     for i in range(1, x + 1):
@@ -42,14 +42,14 @@ def foo_4(x):
     return result
 
 
-def foo_5(x):  # a recursive function that calculates the factorial of x
-    x = float(x)
+def foo_5(x=10):  # a recursive function that calculates the factorial of x
+    x = int(x)
     if x == 1:
         return 1
     return x * foo_5(x - 1)
 
 
-def foo_6(x):  # Calculate the factorial of x in a different way; no if statement involved
+def foo_6(x=10):  # Calculate the factorial of x in a different way; no if statement involved
     x = float(x)
     facto = 1
     while x >= 1:
@@ -60,23 +60,23 @@ def foo_6(x):  # Calculate the factorial of x in a different way; no if statemen
 
 def main(argv):
     if len(argv) == 1:
-        print(foo_1(10))
-        print(foo_2(10, 20))
-        print(foo_3(10, 20, 30))
-        print(foo_4(10))
-        print(foo_5(10))
-        print(foo_6(10))
+        print(foo_1())
+        print(foo_2())
+        print(foo_3())
+        print(foo_4())
+        print(foo_5())
+        print(foo_6())
     elif len(argv) == 2:
         print(foo_1(argv[1]))
-        print(foo_2(argv[1], 20))
-        print(foo_3(argv[1], 20, 30))
+        print(foo_2(argv[1]))
+        print(foo_3(argv[1]))
         print(foo_4(argv[1]))
         print(foo_5(argv[1]))
         print(foo_6(argv[1]))
     elif len(argv) == 3:
         print(foo_1(argv[1]))
         print(foo_2(argv[1], argv[2]))
-        print(foo_3(argv[1], argv[2], 30))
+        print(foo_3(argv[1], argv[2]))
         print(foo_4(argv[1]))
         print(foo_5(argv[1]))
         print(foo_6(argv[1]))
@@ -88,7 +88,7 @@ def main(argv):
         print(foo_5(argv[1]))
         print(foo_6(argv[1]))
     else:
-        print("Too many arguments!")
+        print("Wrong input!")
     return 0
 
 
