8ec4ce522d01f6c1bf1479b9ff8e7bf93d15d7ae
Pu Zhao <pu.zhao23@imperial.ac.uk>
2023-10-19 15:00:34 +0100

    updata week2 files

--- week2/code/cfexercises1.py
+++ week2/code/cfexercises1.py
@@ -10,16 +10,19 @@ import sys
 
 
 def foo_1(x):
+    x = float(x)
     return x ** 0.5
 
 
 def foo_2(x, y):
+    x, y = float(x), float(y)
     if x > y:
         return x
     return y
 
 
 def foo_3(x, y, z):
+    x, y, z = float(x), float(y), float(z)
     if x > y:
         tmp = y
         y = x
@@ -32,6 +35,7 @@ def foo_3(x, y, z):
 
 
 def foo_4(x):
+    x = int(x)
     result = 1
     for i in range(1, x + 1):
         result = result * i
@@ -39,12 +43,14 @@ def foo_4(x):
 
 
 def foo_5(x):  # a recursive function that calculates the factorial of x
+    x = float(x)
     if x == 1:
         return 1
     return x * foo_5(x - 1)
 
 
 def foo_6(x):  # Calculate the factorial of x in a different way; no if statement involved
+    x = float(x)
     facto = 1
     while x >= 1:
         facto = facto * x
@@ -52,7 +58,40 @@ def foo_6(x):  # Calculate the factorial of x in a different way; no if statemen
     return facto
 
 
-print(foo_5(10))
+def main(argv):
+    if len(argv) == 1:
+        print(foo_1(10))
+        print(foo_2(10, 20))
+        print(foo_3(10, 20, 30))
+        print(foo_4(10))
+        print(foo_5(10))
+        print(foo_6(10))
+    elif len(argv) == 2:
+        print(foo_1(argv[1]))
+        print(foo_2(argv[1], 20))
+        print(foo_3(argv[1], 20, 30))
+        print(foo_4(argv[1]))
+        print(foo_5(argv[1]))
+        print(foo_6(argv[1]))
+    elif len(argv) == 3:
+        print(foo_1(argv[1]))
+        print(foo_2(argv[1], argv[2]))
+        print(foo_3(argv[1], argv[2], 30))
+        print(foo_4(argv[1]))
+        print(foo_5(argv[1]))
+        print(foo_6(argv[1]))
+    elif len(argv) == 4:
+        print(foo_1(argv[1]))
+        print(foo_2(argv[1], argv[2]))
+        print(foo_3(argv[1], argv[2], argv[3]))
+        print(foo_4(argv[1]))
+        print(foo_5(argv[1]))
+        print(foo_6(argv[1]))
+    else:
+        print("Too many arguments!")
+    return 0
+
 
 if __name__ == "__main__":
-    sys.exit(0)
+    status = main(sys.argv)
+    exit(status)
